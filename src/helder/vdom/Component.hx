4d47cff3ef1eac09970689b68daeecdec166b672
Ben Merckx <ben@codeurs.be>
2018-08-09 19:23:38 +0200

    Cleanup

--- src/helder/vdom/Component.hx
+++ src/helder/vdom/Component.hx
@@ -3,23 +3,31 @@ package helder.vdom;
 #if macro
 import haxe.macro.Expr;
 import haxe.macro.Context;
+import haxe.macro.Type;
 using tink.MacroApi;
 #else
-@:genericBuild(helder.vdom.macro.ComponentBuilder.build())
-class Component<Rest> {}
+@:genericBuild(helder.vdom.Component.setDefaults())
 #end
-
-class ComponentImpl<Props: {}> {
-  public function new() {}
-
-  macro function h(ethis: Expr, selector: Expr, ?attrs: Expr, ?children: Array<Expr>) {
-    switch selector.getString() {
-      case Success(selector): 
-        trace(selector);
-      default: 
-        var type = Context.getType(selector.toString());
-        return selector;
+class Component<Rest> {
+  #if macro
+  static var object = (macro: {}).toType().sure();
+  static function setDefaults()
+    return switch Context.getLocalType() {
+      case type = TInst(_.toString() == 'helder.vdom.Component' => true, params):
+        'helder.vdom.Component.ComponentWrapper'.asComplexType((
+          switch params {
+            case [p]: [p, object];
+            case [p, s]: [p, s];
+            default: [object, object];
+          }
+        ).map(function (type) return TPType(type.toComplex())));
+      default:
+        throw 'assert';
     }
-    return macro 'root comp';
-  }
+  #end
 }
+
+typedef ComponentWrapper<Props: {}, State: {}> =
+  #if mithril helder.vdom.wrapper.MithrilWrapper<Props, State>
+  #elseif react helder.vdom.wrapper.ReactWrapper<Props, State>
+  #else #error 'No virtual dom library selected' #end
